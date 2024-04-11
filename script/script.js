07d628508145524d38b8a088a090422d87e02324
Oleg <akademuk24@gmail.com>
2024-04-11 13:29:11 +0300

    add hero, categories, promotion-banner page

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 setupMenu();
 setupSearch();
+initializeModal();
 
 
 
@@ -42,7 +43,17 @@ function setupMenu() {
         menu.classList.remove('active');
         body.classList.remove('active');
     });
+
+    menu.addEventListener('click', function(event) {
+        if (event.target.classList.contains('menu__link')) {
+            menuBtn.classList.remove('active');
+            menu.classList.remove('active');
+            body.classList.remove('active');
+        }
+    });
+    
 }
+
 // SEARCH
 function setupSearch() {
     let searchOpenBtn = document.querySelector('.search-open');
@@ -76,6 +87,171 @@ function setupSearch() {
     });
 }
 
+// Authorization LOGIN / SIGN UP
+function initializeModal() {
+    const modal = document.querySelector('.authorization');
+    const openBtn = document.querySelector('.authorization-open');
+    const closeBtn = document.querySelector('.authorization__close');
+    const loginBtn = document.querySelector('.authorization__login-btn');
+    const signUpBtn = document.querySelector('.authorization__sign-up-btn');
+    const loginModal = document.querySelector('.authorization__login');
+    const signUpModal = document.querySelector('.authorization__sign-up');
+    let lastOpenModal = '.authorization__sign-up'; // Устанавливаем первоначальное значение
 
+    function closeModal() {
+        modal.classList.remove('show-modal');
+    }
 
+    function handleModalClick(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    }
 
+    function handleEscKey(event) {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    }
+
+    openBtn.addEventListener('click', function() {
+        modal.classList.add('show-modal');
+        if (lastOpenModal === '.authorization__login') {
+            signUpModal.style.display = 'none';
+            loginModal.style.display = 'block';
+        } else if (lastOpenModal === '.authorization__sign-up') {
+            loginModal.style.display = 'none';
+            signUpModal.style.display = 'block';
+        } else {
+            signUpModal.style.display = 'block'; // Показываем модальное окно регистрации при открытии
+        }
+    });
+
+    closeBtn.addEventListener('click', closeModal);
+
+    loginBtn.addEventListener('click', function() {
+        loginModal.style.display = 'none';
+        signUpModal.style.display = 'block';
+        lastOpenModal = '.authorization__sign-up';
+    });
+
+    signUpBtn.addEventListener('click', function() {
+        signUpModal.style.display = 'none';
+        loginModal.style.display = 'block';
+        lastOpenModal = '.authorization__login';
+    });
+
+    document.addEventListener('click', handleModalClick);
+    document.addEventListener('keyup', handleEscKey);
+}
+
+// Validation LOGIN / SIGN UP
+document.addEventListener('DOMContentLoaded', function() {
+    function togglePasswordVisibility(inputId) {
+        const passwordInput = document.getElementById(inputId);
+        const passwordToggle = document.querySelector(`#${inputId} + .password-toggle img`);
+
+        if (passwordInput && passwordToggle) {
+            passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+            passwordToggle.src = passwordInput.type === "password" ? "img/open-pasword.svg" : "img/open-pasword.svg";
+        }
+    }
+
+    function validateAndSubmit(formId) {
+        const form = document.getElementById(formId);
+        if (!form) return; // Проверка наличия формы
+
+        const fields = [
+            { id: `${formId}-email`, validationMessageId: `${formId}-email-validation`, errorMessage: 'Please enter a valid email address' },
+            { id: `${formId}-password`, validationMessageId: `${formId}-password-validation`, errorMessage: 'Password must be at least 8 characters long' },
+            { id: `${formId}-repeat-password`, validationMessageId: `${formId}-repeat-password-validation`, errorMessage: 'Password must be at least 8 characters long' }
+        ];
+
+        let isValid = true;
+        let password = ''; // Variable to store password value
+
+        fields.forEach(field => {
+            const input = document.getElementById(field.id);
+            const validationMessage = document.getElementById(field.validationMessageId);
+
+            if (input && validationMessage) {
+                if (field.id === `${formId}-password`) {
+                    password = input.value.trim(); // Save password value
+                }
+
+                if (field.id === `${formId}-repeat-password`) {
+                    // Check password match
+                    if (input.value.trim() !== password) {
+                        validationMessage.textContent = 'Passwords do not match';
+                        isValid = false;
+                    }
+                } else {
+                    if (input.value.trim() === '' || !input.checkValidity()) {
+                        validationMessage.textContent = field.errorMessage;
+                        isValid = false;
+                    } else {
+                        validationMessage.textContent = '';
+                    }
+                }
+            }
+        });
+
+        if (isValid) {
+            form.submit();
+        }
+    }
+
+    function clearValidationMessage(fieldId) {
+        const validationMessage = document.getElementById(`${fieldId}-validation`);
+        if (validationMessage) {
+            validationMessage.textContent = '';
+        }
+    }
+
+    ['signUpForm', 'loginForm'].forEach(formId => {
+        const form = document.getElementById(formId);
+        if (form) {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+                validateAndSubmit(formId); // Валидация и отправка формы
+            });
+
+            const fields = ['email', 'password', 'repeat-password'];
+            fields.forEach(fieldId => {
+                const fieldInput = document.getElementById(`${formId}-${fieldId}`);
+                if (fieldInput) {
+                    fieldInput.addEventListener('input', function() {
+                        clearValidationMessage(`${formId}-${fieldId}`); // Очистка сообщения об ошибке валидации
+                    });
+                }
+            });
+        }
+    });
+});
+
+// HERO slider
+var swiper = new Swiper(".hero__swiper", {
+    spaceBetween: 0,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".promotion-banner__swiper", {
+    spaceBetween: 0,
+    effect: "fade",
+    navigation: {
+      nextEl: ".promotion-banner-next",
+      prevEl: ".promotion-banner-prev",
+    },
+    pagination: {
+      el: ".promotion-banner-pagination",
+      clickable: true,
+    },
+});
